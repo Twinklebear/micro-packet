959b1450872ac7711d464233394aea47f6ec8c17
Will Usher <willusher.life@gmail.com>
2015-03-20 16:35:50 -0600

    Accidently used division op when meaning _m256_div_ps

--- src/plane.cpp
+++ src/plane.cpp
@@ -4,7 +4,7 @@ Plane::Plane(Vec3f pos, Vec3f normal) : pos(pos), normal(normal){}
 __m256 Plane::intersect(Ray8 &ray) const {
 	const auto vpos = Vec3f_8{pos};
 	const auto vnorm = Vec3f_8{normal};
-	const auto t = (vpos - ray.o).dot(vnorm) / ray.d.dot(vnorm);
+	const auto t = _mm256_div_ps((vpos - ray.o).dot(vnorm), ray.d.dot(vnorm));
 	auto hits = _mm256_and_ps(_mm256_cmp_ps(t, ray.t_min, _CMP_GT_OQ),
 			_mm256_cmp_ps(t, ray.t_max, _CMP_LT_OQ));
 	// Check if all rays miss the sphere
