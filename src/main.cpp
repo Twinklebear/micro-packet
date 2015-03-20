a9b69a07f7e0db432cb076a8ddc1e6f4c08407bb
Will Usher <willusher.life@gmail.com>
2015-03-19 23:21:38 -0600

    Buggy, broken camera code implemented

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,5 @@
+#define _USE_MATH_DEFINES
+#include <cmath>
 #include <iostream>
 #include <array>
 #include <iomanip>
@@ -44,54 +46,136 @@ __m256 solve_quadratic(const __m256 a, const __m256 b, const __m256 c, __m256 &t
 	return solved;
 }
 
-// Struct holding 8 vec3's
-struct Vec8 {
+// A single vec3f
+struct Vec3f {
+	float x, y, z;
+
+	Vec3f(float x = 0, float y = 0, float z = 0) : x(x), y(y), z(z){}
+	inline float dot(const Vec3f &v) const {
+		return x * v.x + y * v.y + z * v.z;
+	}
+	inline Vec3f cross(const Vec3f &v) const {
+		return Vec3f{y * v.z - z * v.y, z * v.x - x * v.z, x * v.y - y * v.x};
+	}
+	inline float length_sqr() const {
+		return x * x + y * y + z * z;
+	}
+	inline float length() const {
+		return std::sqrt(length_sqr());
+	}
+	inline Vec3f normalized() const {
+		const float len = length();
+		return Vec3f{x / len, y / len, z / len};
+	}
+};
+inline Vec3f operator+(const Vec3f &a, const Vec3f &b){
+	return Vec3f{a.x + b.x, a.y + b.y,  a.z + b.z};
+}
+inline Vec3f operator-(const Vec3f &a, const Vec3f &b){
+	return Vec3f{a.x - b.x, a.y - b.y,  a.z - b.z};
+}
+inline Vec3f operator*(const Vec3f &v, float s){
+	return Vec3f{v.x * s, v.y * s, v.z * s};
+}
+inline Vec3f operator*(float s, const Vec3f &v){
+	return v * s;
+}
+inline Vec3f operator/(const Vec3f &v, float s){
+	return v * (1 / s);
+}
+inline Vec3f operator/(const Vec3f &a, const Vec3f &b){
+	return Vec3f{a.x / b.x, a.y / b.y, a.z / b.z};
+}
+inline Vec3f operator-(const Vec3f &v){
+	return Vec3f{-v.x, -v.y, -v.z};
+}
+std::ostream& operator<<(std::ostream &os, const Vec3f &v){
+	std::cout << "Vec3f:\n\tx = " << v.x
+		<< "\n\ty = " << v.y
+		<< "\n\tz = " << v.z;
+	return os;
+}
+
+// Struct holding 8 vec3f's
+struct Vec3f_8 {
 	__m256 x, y, z;
 
-	Vec8(float x = 0, float y = 0, float z = 0)
-		: x(_mm256_set1_ps(x)), y(_mm256_set1_ps(y)), z(_mm256_set1_ps(z))
-	{}
-	Vec8(__m256 x, __m256 y, __m256 z) : x(x), y(y), z(z){}
+	Vec3f_8(float x = 0, float y = 0, float z = 0)
+		: x(_mm256_set1_ps(x)), y(_mm256_set1_ps(y)), z(_mm256_set1_ps(z)){}
+	Vec3f_8(__m256 x, __m256 y, __m256 z) : x(x), y(y), z(z){}
 	// Compute length^2 of all 8 vectors
-	__m256 length_sqr() const {
+	inline __m256 length_sqr() const {
 		const auto x_sqr = _mm256_mul_ps(x, x);
 		const auto y_sqr = _mm256_mul_ps(y, y);
 		const auto z_sqr = _mm256_mul_ps(z, z);
 		return _mm256_add_ps(x_sqr, _mm256_add_ps(y_sqr, z_sqr));
 	}
 	// Compute length of all 8 vectors
-	__m256 length() const {
+	inline __m256 length() const {
 		return _mm256_sqrt_ps(length_sqr());
 	}
-	__m256 dot(const Vec8 &vb) const {
+	// Normalize all 8 vectors
+	inline void normalize(){
+		const auto len = length();
+		x = _mm256_div_ps(x, len);
+		y = _mm256_div_ps(y, len);
+		z = _mm256_div_ps(z, len);
+	}
+	inline __m256 dot(const Vec3f_8 &vb) const {
 		const auto a = _mm256_mul_ps(x, vb.x);
 		const auto b = _mm256_mul_ps(y, vb.y);
 		const auto c = _mm256_mul_ps(z, vb.z);
 		return _mm256_add_ps(a, _mm256_add_ps(b, c));
 	}
 };
-Vec8 operator-(const Vec8 &a, const Vec8 &b){
-	return Vec8{_mm256_sub_ps(a.x, b.x), _mm256_sub_ps(a.y, b.y),
+inline Vec3f_8 operator+(const Vec3f_8 &a, const Vec3f_8 &b){
+	return Vec3f_8{_mm256_add_ps(a.x, b.x), _mm256_add_ps(a.y, b.y), _mm256_add_ps(a.z, b.z)};
+}
+inline Vec3f_8 operator-(const Vec3f_8 &a, const Vec3f_8 &b){
+	return Vec3f_8{_mm256_sub_ps(a.x, b.x), _mm256_sub_ps(a.y, b.y),
 		_mm256_sub_ps(a.z, b.z)};
 }
-
-std::ostream& operator<<(std::ostream &os, const Vec8 &v){
-	os << "Vec8:\n\tx = " << v.x
+// Scale all components of the vector
+inline Vec3f_8 operator*(__m256 s, const Vec3f_8 &v){
+	return Vec3f_8{_mm256_mul_ps(s, v.x), _mm256_mul_ps(s, v.y), _mm256_mul_ps(s, v.z)};
+}
+std::ostream& operator<<(std::ostream &os, const Vec3f_8 &v){
+	os << "Vec3f_8:\n\tx = " << v.x
 		<< "\n\ty = " << v.y
-		<< "\n\tz = " << v.z
-		<< "\n";
+		<< "\n\tz = " << v.z;
 	return os;
 }
 
+// Struct holding 8 vec2i's
+struct Vec2f_8 {
+	__m256 x, y;
+
+	Vec2f_8(float x = 0, float y = 0) : x(_mm256_set1_ps(x)), y(_mm256_set1_ps(y)){}
+	Vec2f_8(__m256 x, __m256 y) : x(x), y(y){}
+};
+std::ostream& operator<<(std::ostream &os, const Vec2f_8 &v){
+	os << "Vec2f_8:\n\tx = " << v.x
+		<< "\n\ty = " << v.y;
+	return os;
+}
+
+
 // Packet of 8 rays
 struct Ray8 {
-	Vec8 o, d;
+	Vec3f_8 o, d;
 	__m256 t_max, t_min;
 
-	Ray8(Vec8 o, Vec8 d) : o(o), d(d), t_max(_mm256_set1_ps(FLT_MAX)),
+	Ray8(Vec3f_8 o = Vec3f_8{}, Vec3f_8 d = Vec3f_8{}) : o(o), d(d), t_max(_mm256_set1_ps(INFINITY)),
 		t_min(_mm256_set1_ps(0))
 	{}
 };
+std::ostream& operator<<(std::ostream &os, const Ray8 &r){
+	os << "Ray8:\no = " << r.o
+		<< "\nd = " << r.d
+		<< "\nt_max = " << r.t_max
+		<< "\nt_min = " << r.t_min;
+	return os;
+}
 
 struct Sphere {
 	float x, y, z, radius;
@@ -99,7 +183,7 @@ struct Sphere {
 	Sphere(float x, float y, float z, float radius) : x(x), y(y), z(z), radius(radius) {}
 	// Test 8 rays against the sphere, returns masks for the hits (0xff) and misses (0x00)
 	__m256 intersect(Ray8 &ray) const {
-		const auto center = Vec8{x, y, z};
+		const auto center = Vec3f_8{x, y, z};
 		const auto d = center - ray.o;
 		const auto a = ray.d.length_sqr();
 		const auto b = _mm256_mul_ps(ray.d.dot(d), _mm256_set1_ps(-2.f));
@@ -118,7 +202,7 @@ struct Sphere {
 			<< "\nt0 = " << t0
 			<< "\nt1 = " << t1 << std::endl;
 		// We want t0 to hold the nearest t value that is greater than ray.t_min
-		auto swap_t = _mm256_cmp_ps(t0, ray.t_min, _CMP_GT_OQ);
+		auto swap_t = _mm256_cmp_ps(t0, ray.t_min, _CMP_LT_OQ);
 		t0 = _mm256_blendv_ps(t0, t1, swap_t);
 		// Check which rays are within the ray's t range
 		auto in_range = _mm256_and_ps(_mm256_cmp_ps(t0, ray.t_min, _CMP_GT_OQ),
@@ -134,25 +218,59 @@ struct Sphere {
 	}
 };
 
+// Simple perspective camera. Perhaps later switch to have transformation matrices?
+// would make it easier to have interactive rendering and could add in my glt arball camera
+struct PerspectiveCamera {
+	// dir_top_left is the direction from the camera to the top left of the image
+	Vec3f pos, dir, up, dir_top_left, screen_du, screen_dv;
+
+	PerspectiveCamera(Vec3f pos, Vec3f center, Vec3f up, float fovy, float aspect)
+		: pos(pos), dir((center - pos).normalized()), up(up)
+	{
+		Vec3f dz = dir.normalized();
+		Vec3f dx = -dz.cross(up).normalized();
+		Vec3f dy = dx.cross(dz).normalized();
+		float dim_y = 2.f * std::sin((fovy / 2.f) * (M_PI / 180.f));
+		float dim_x = dim_y * aspect;
+		dir_top_left = dz - 0.5f * dim_x * dx - 0.5f * dim_y * dy;
+		screen_du = dx * dim_x;
+		screen_dv = dy * dim_y;
+		std::cout << "dir_top_left = " << dir_top_left
+			<< "\nscreen_du = " << screen_du
+			<< "\nscreen_dv = " << screen_dv << "\n";
+	}
+	// Generate a ray packet sampling the 8 screen positions passed
+	void generate_rays(Ray8 &rays, const Vec2f_8 &samples) const {
+		rays.o = Vec3f_8{pos.x, pos.y, pos.z};
+		rays.d = Vec3f_8{dir_top_left.x, dir_top_left.y, dir_top_left.z};
+		const auto u_step = samples.x * Vec3f_8{screen_du.x, screen_du.y, screen_du.z};
+		const auto v_step = samples.y * Vec3f_8{screen_dv.x, screen_dv.y, screen_dv.z};
+		std::cout << "u_step = " << u_step
+			<< "\nv_step = " << v_step
+			<< "\nrays.d = " << rays.d << "\n";
+		rays.d = rays.d + u_step + v_step;
+		std::cout << "final (not normalized ray dir) = " << rays.d << "\n";
+		rays.d.normalize();
+		rays.t_min = _mm256_set1_ps(0);
+		rays.t_max = _mm256_set1_ps(INFINITY);
+	}
+};
+
 int main(int, char**){
-	const auto sphere = Sphere{0, 1, 0, 1};
-	std::cout << "All hit packet:\n";
-	auto packet = Ray8{Vec8{0, 1, -2}, Vec8{0, 0, 1}};
+	const auto sphere = Sphere{0, 0, 0, 2};
+	const auto camera = PerspectiveCamera{Vec3f{0, 0, -5}, Vec3f{0, 0, 0}, Vec3f{0, 1, 0}, 30.f, 1.f};
+	std::array<float, 8> pixel_x, pixel_y;
+	for (int i = 0; i < 8; ++i){
+		pixel_x[i] = i % 4;
+		pixel_y[i] = i / 4;
+	}
+	const auto samples = Vec2f_8{_mm256_loadu_ps(pixel_x.data()), _mm256_loadu_ps(pixel_y.data())};
+	std::cout << "Samples = " << samples << "\n";
+	Ray8 packet;
+	camera.generate_rays(packet, samples);
+	std::cout << "packet = " << packet << std::endl;
 	auto hits = sphere.intersect(packet);
-	std::cout << "hits = " << hits << std::endl;
-
-	std::cout << "\nAll miss packet:\n";
-	packet = Ray8{Vec8{0, 1, -2}, Vec8{0, 0, -1}};
-	hits = sphere.intersect(packet);
-	std::cout << "hits = " << hits << std::endl;
-
-	std::cout << "\nMixed packet, expect first two to hit:\n";
-	const std::array<float, 8> dir_x{0, 0, 1, -1, 1, -1, 1, 0};
-	const std::array<float, 8> dir_y{1, 0.25, 0, 0, 1, 1, -1, 0};
-	auto dir_z = _mm256_set1_ps(1);
-	auto ray_dirs = Vec8{_mm256_loadu_ps(dir_x.data()), _mm256_loadu_ps(dir_y.data()), dir_z};
-	packet = Ray8{Vec8{0, 0, -2}, ray_dirs};
-	hits = sphere.intersect(packet);
-	std::cout << "hits = " << hits << std::endl;
+	std::cout << "Actual hits = " << hits << std::endl;
+	std::cout << "packet post hit: " << packet << std::endl;
 }
 
