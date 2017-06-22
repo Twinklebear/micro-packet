1455f8d8808af6c0bffa0715692424c40641b092
Will Usher <will.usher@intel.com>
2017-06-22 12:40:21 -0500

    convert material

--- include/material.h
+++ include/material.h
@@ -5,15 +5,15 @@
 #include "color.h"
 
 struct Material {
-	virtual Colorf_8 shade(const Vec3f_8 &w_o, const Vec3f_8 &w_i) const = 0;
+	virtual ColorfN shade(const Vec3fN &w_o, const Vec3fN &w_i) const = 0;
 };
 
 struct LambertianMaterial : Material {
 	Colorf color;
 
 	inline LambertianMaterial(Colorf color) : color(color){}
-	inline Colorf_8 shade(const Vec3f_8&, const Vec3f_8&) const {
-		return Colorf_8{color * static_cast<float>(M_1_PI)};
+	inline ColorfN shade(const Vec3fN&, const Vec3fN&) const {
+		return ColorfN{color * static_cast<float>(M_1_PI)};
 	}
 };
 
