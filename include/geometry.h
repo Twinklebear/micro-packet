93f94ae626d21184ca1150f409ca1e6ad10b185d
Will Usher <will.usher@intel.com>
2017-06-22 12:38:08 -0500

    convert geom base class

--- include/geometry.h
+++ include/geometry.h
@@ -1,14 +1,13 @@
 #ifndef GEOMETRY_H
 #define GEOMETRY_H
 
-#include "immintrin.h"
 #include "diff_geom.h"
 
 struct Geometry {
 	/*
 	 * Test a ray packet for intersection against the object
 	 */
-	virtual __m256 intersect(Ray8 &ray, DiffGeom8 &dg) const = 0;
+	virtual psimd::mask intersect(RayN &ray, DiffGeomN &dg) const = 0;
 };
 
 #endif
