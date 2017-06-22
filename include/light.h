8b274c50daa623369b5f5dc3eb4277127abc9262
Will Usher <will.usher@intel.com>
2017-06-22 12:39:52 -0500

    convert light

--- include/light.h
+++ include/light.h
@@ -16,7 +16,7 @@ struct PointLight {
 	 * returns the incident direction of the light and an occlusion tester
 	 * that can be used to check if the light is visble
 	 */
-	Colorf_8 sample(const Vec3f_8 &p, Vec3f_8 &w_i, OcclusionTester &occlusion) const;
+	ColorfN sample(const Vec3fN &p, Vec3fN &w_i, OcclusionTester &occlusion) const;
 };
 
 #endif
--- src/light.cpp
+++ src/light.cpp
@@ -2,11 +2,11 @@
 #include "occlusion_tester.h"
 
 PointLight::PointLight(Vec3f pos, Colorf intensity) : pos(pos), intensity(intensity){}
-Colorf_8 PointLight::sample(const Vec3f_8 &p, Vec3f_8 &w_i, OcclusionTester &occlusion) const {
+ColorfN PointLight::sample(const Vec3fN &p, Vec3fN &w_i, OcclusionTester &occlusion) const {
 	occlusion.set_points(p, pos);
 	w_i = (pos - p);
 	const auto dist_sqr = w_i.length_sqr();
 	w_i.normalize();
-	return Colorf_8{intensity} / dist_sqr;
+	return ColorfN{intensity} / dist_sqr;
 }
 
